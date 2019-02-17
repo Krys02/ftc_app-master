d732458759bdda587da8f5fd5483b579cb1f329c
mpilzer <maxwellpilzer@gmail.com>
2019-02-16 23:15:09 -0700

    Started gyro code, still need to add cases for overflow methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedCraterAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedCraterAuto.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import com.opencv.checkmatecv.CameraViewDisplay;
+import com.opencv.checkmatecv.OpenCV;
+import com.opencv.checkmatecv.detectors.roverrukus.GoldAlignDetector;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -12,7 +15,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name="Safe Autonomous", group="Autonomous")
+@Autonomous(name="Red Crater Autonomous", group="Autonomous")
 public class RedCraterAuto extends LinearOpMode {
 
     private String autoPath = "CRATER";
@@ -23,7 +26,8 @@ public class RedCraterAuto extends LinearOpMode {
     private RevBlinkinLedDriver.BlinkinPattern off = RevBlinkinLedDriver.BlinkinPattern.BLACK;
     private RevBlinkinLedDriver.BlinkinPattern white = RevBlinkinLedDriver.BlinkinPattern.WHITE;
 
-    CMHardware robot = new CMHardware();
+    private CMHardware robot = new CMHardware();
+    private GoldAlignDetector detector;
 
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -31,8 +35,11 @@ public class RedCraterAuto extends LinearOpMode {
     private static final double     COUNTS_PER_INCH         = (868.4) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
 
-    Orientation lastAngles = new Orientation();
-    double globalAngle, power = .30, correction;
+    // IMU Variables
+    private Orientation angles;
+    private double prevAngle;
+    private double targetHeading;
+    private String overflow;
 
     @Override
     public void runOpMode() {
@@ -49,59 +56,102 @@ public class RedCraterAuto extends LinearOpMode {
                 tertiaryBlinkinColor = RevBlinkinLedDriver.BlinkinPattern.GREEN;
         }
 
+        teleUpdate("Initializing Electronics");
+        robot.init(hardwareMap, false, true, true);
+
+        teleUpdate("Testing Primary Blinkin Color");
         robot.blinkin.setPattern(defaultBlinkinColor);
         sleep (500);
+        teleUpdate("Testing Secondary Blinkin Color");
         robot.blinkin.setPattern(secondaryBlinkinColor);
         sleep (500);
+        teleUpdate("Testing Tertiary Blinkin Color");
         robot.blinkin.setPattern(tertiaryBlinkinColor);
         sleep (500);
         robot.blinkin.setPattern(off);
 
-        telemetry.addData("Status:", "Initializing Electronics");
-        telemetry.update();
-        robot.init(hardwareMap, false, true, true);
-
         while (!isStopRequested() && !robot.imu.isGyroCalibrated()) {
             sleep(50);
             idle();
-            telemetry.addData("Status:", "Calibrating IMU");
-            telemetry.update();
+            teleUpdate("Calibrating IMU");
         }
+        teleUpdate("Setting up OpenCV Detector");
+        detector = new GoldAlignDetector();
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        detector.useDefaults();
+
+        teleUpdate("Tuning OpenCV Detector");
+        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned.
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.4;
+        sleep(200);
+
+        teleUpdate("Tuning OpenCV Scorer");
+        detector.areaScoringMethod = OpenCV.AreaScoringMethod.MAX_AREA;
+        //detector.perfectAreaScorer.perfectArea = 10000;
+        detector.maxAreaScorer.weight = 0.005;
+        sleep(100);
+
+        teleUpdate("Adjusting OpenCV Scorer");
+        detector.ratioScorer.weight = 5; //
+        detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+        sleep(100);
+
+        teleUpdate("Enabling OpenCV");
+        detector.enable();
+        sleep(100);
 
         while (!isStarted() && !isStopRequested()){
-            telemetry.addData("Status:", "Initialized, ready to run");
-            telemetry.update();
+            teleUpdate("Initialization Complete, waiting for start");
         }
 
         waitForStart();
 
-
         if (opModeIsActive() && !isStopRequested()){
-            telemetry.addData("Status:", "Ejecting Stop");
-            telemetry.update();
+            teleUpdate("Ejecting Stop");
             robot.blinkin.setPattern(white);
             // DRIVE MOTORS BACK TO EJECT STOP
             robot.armPower(0.5);
-            sleep(300);
+            sleep(200);
             robot.armPower(0);
-            telemetry.addData("Status:", "Landing");
-            telemetry.update();
+            teleUpdate("Landing");
             robot.arm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             robot.arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            while (robot.potentiometer.getVoltage() < 1.05 && !isStopRequested() && opModeIsActive()){
+            while (robot.potentiometer.getVoltage() > 1.36 && !isStopRequested() && opModeIsActive()){
                 robot.armPower(-0.5);
             }
             robot.armPower(0);
         }
 
+        teleUpdate("Landed");
 
+        robot.intakePosition(0.3);
+        teleUpdate("Strafing off Latch");
+        encoderStrafe(1, -8, -8, 5);
+        teleUpdate("Driving Forward");
+        encoderDrive(1, 8, 8, 5);
+
+        teleUpdate("Lowering Arm");
+        while (robot.potentiometer.getVoltage() < 2.075 && !isStopRequested() && opModeIsActive()){
+            robot.armPower(0.5);
+        }
+        robot.armPower(0);
+        teleUpdate("Strafing Back to Center");
+        encoderStrafe(1, 8, 8, 5);
+        teleUpdate("Driving Forward");
+        encoderDrive(1, 6, 6, 5);
+        teleUpdate("Rotating");
+        while (Math.abs(angles.firstAngle) < 175){
+            angles  = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            telemetry.addData("Status","Rotating");
+            telemetry.addData("Heading", angles.firstAngle);
+            telemetry.update();
+            robot.motorRotate(0.2);
+        }
+        robot.driveMotorPower(0);
 
     }
 
-    private void waitForSeconds(float seconds){
-        int millisecondsToWait = Math.round(seconds*1000);
-        sleep(millisecondsToWait);
-    }
     public void encoderDrive(double speed,
                              double leftInches, double rightInches,
                              double timeoutS) {
@@ -120,6 +170,8 @@ public class RedCraterAuto extends LinearOpMode {
             newBackRightTarget = robot.backRightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
             robot.leftDrive.setTargetPosition(newLeftTarget);
             robot.rightDrive.setTargetPosition(newRightTarget);
+            robot.backLeftDrive.setTargetPosition(newBackLeftTarget);
+            robot.backRightDrive.setTargetPosition(newBackRightTarget);
 
             // Turn On RUN_TO_POSITION
             robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -177,33 +229,35 @@ public class RedCraterAuto extends LinearOpMode {
             newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (frontInches * COUNTS_PER_INCH / 2);
             newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (-frontInches * COUNTS_PER_INCH / 2);
             newBackLeftTarget = robot.backLeftDrive.getCurrentPosition() + (int) (-backInches * COUNTS_PER_INCH / 2);
-            newBackRightTarget = robot.actuator2.getCurrentPosition() + (int) (backInches * COUNTS_PER_INCH / 2);
+            newBackRightTarget = robot.backRightDrive.getCurrentPosition() + (int) (backInches * COUNTS_PER_INCH / 2);
             robot.leftDrive.setTargetPosition(newLeftTarget);
             robot.rightDrive.setTargetPosition(newRightTarget);
             robot.backLeftDrive.setTargetPosition(newBackLeftTarget);
-            robot.actuator2.setTargetPosition(newBackRightTarget);
+            robot.backRightDrive.setTargetPosition(newBackRightTarget);
 
             // Turn On RUN_TO_POSITION
             robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             robot.backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.actuator2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             // reset the timeout time and start motion.
             runtime.reset();
             robot.leftDrive.setPower(Math.abs(speed));
             robot.rightDrive.setPower(Math.abs(speed));
             robot.backLeftDrive.setPower(Math.abs(speed));
-            robot.actuator2.setPower(Math.abs(speed));
+            robot.backRightDrive.setPower(Math.abs(speed));
 
             while (opModeIsActive() &&
                     (runtime.seconds() < timeoutS) &&
                     (robot.leftDrive.isBusy() && robot.rightDrive.isBusy() &&
-                            robot.backLeftDrive.isBusy() && robot.actuator2.isBusy()) && !isStopRequested()) {
+                            robot.backLeftDrive.isBusy() && robot.backRightDrive.isBusy()) && !isStopRequested()) {
 
                 if (isStopRequested() || !opModeIsActive()) {
                     robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                     robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    robot.backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    robot.backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                     robot.driveMotorPower(0);
                 }
             }
@@ -218,64 +272,58 @@ public class RedCraterAuto extends LinearOpMode {
             robot.backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         }
     }
-    private void resetAngle()
-    {
-        lastAngles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    private double adjustedHeading(){
+        angles  = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return (angles.firstAngle + 180);
 
-        globalAngle = 0;
     }
-    private double getAngle()
-    {
-        Orientation angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    private void rotate(double degrees, double speed){
+        prevAngle = adjustedHeading();
+        telemetry.addData("Status:", "Rotating");
+        telemetry.addData("Last Heading:", prevAngle);
+        if ((prevAngle + degrees) < 360 && (prevAngle + degrees) >= 0){
+            targetHeading = prevAngle + degrees;
+            overflow = "NONE";
+        } else if ((prevAngle + degrees) >= 360){
+            targetHeading = (prevAngle + degrees) - 360;
+            overflow = "UPPER";
+        } else if ((prevAngle + degrees) < 0){
+            targetHeading = (prevAngle + degrees) + 360;
+            overflow = "LOWER";
+        }
+        telemetry.addData("Target Heading:", targetHeading);
+        telemetry.addData("Rotation Overflow:", overflow);
+        telemetry.update();
+        sleep(50);
+        if (overflow.equals("NONE")) { // No weird calculations needed if no overflow
+            if (adjustedHeading() < targetHeading) { // we need to rotate clockwise
+                while (opModeIsActive() && !isStopRequested() && adjustedHeading() < targetHeading) {
+                    robot.motorRotate(-speed);
+                    telemetry.addData("Target Heading:", targetHeading);
+                    telemetry.addData("Current Heading:", adjustedHeading());
+                    telemetry.update();
+                }
+            }
+            if (adjustedHeading() > targetHeading) { // we need to rotate counterclockwise
+                while (opModeIsActive() && !isStopRequested() && adjustedHeading() < targetHeading) {
+                    robot.motorRotate(speed);
+                    telemetry.addData("Target Heading:", targetHeading);
+                    telemetry.addData("Current Heading:", adjustedHeading());
+                    telemetry.update();
+                }
+            }
+        }
+        if (overflow.equals("UPPER")){
 
-        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+        }
 
-        if (deltaAngle < -180)
-            deltaAngle += 360;
-        else if (deltaAngle > 180)
-            deltaAngle -= 360;
 
-        globalAngle += deltaAngle;
 
-        lastAngles = angles;
-
-        return globalAngle;
     }
-    private void rotate(int degrees, double power)
-    {
-        double  leftPower, rightPower;
 
-        resetAngle();
-
-        if (degrees < 0)
-        {   // turn right.
-            leftPower = -power;
-            rightPower = power;
-        }
-        else if (degrees > 0)
-        {   // turn left.
-            leftPower = power;
-            rightPower = -power;
-        }
-        else return;
-        // set power to rotate.
-        robot.leftDrive.setPower(leftPower);
-        robot.rightDrive.setPower(rightPower);
-        robot.backLeftDrive.setPower(leftPower);
-        robot.backRightDrive.setPower(rightPower);
-
-        // rotate until turn is completed.
-        if (degrees < 0)
-        {
-            while (opModeIsActive() && getAngle() == 0) {}
-            while (opModeIsActive() && getAngle() > degrees) {}
-        }
-        else    // left turn.
-            while (opModeIsActive() && getAngle() < degrees) {}
-        // turn the motors off.
-        robot.driveMotorPower(0);
-        sleep(500);
-        resetAngle();
+    private void teleUpdate(String status){
+        telemetry.addData("Status:", status);
+        telemetry.update();
     }
 
 }
